992e69af08ecb71188ef16ae83250761fcd55514
Cartiilov <92887460+Cartiilov@users.noreply.github.com>
2022-03-27 11:09:01 +0200

    troche nie dziala

--- ProjectWidget/Source.cpp
+++ ProjectWidget/Source.cpp
@@ -24,6 +24,7 @@ private:
 	wxBoxSizer* WxBoxSizer2;
 	wxPanel* WxPanel1;
 	wxBoxSizer* WxBoxSizer1;
+	wxBoxSizer* WxBoxSizer3;
 
 
 
@@ -31,6 +32,7 @@ private:
 	void WxEdit1_Updated(wxCommandEvent& e);
 	void WxCheckBox1_Click(wxCommandEvent& e);
 	void WxScrollBar1_Scroll(wxScrollEvent& e);
+	void Form_Paint(wxPaintEvent& e);
 
 	enum
 	{
@@ -70,34 +72,40 @@ MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "GFK Lab03")
 	this->SetAutoLayout(true);
 
 	WxPanel1 = new wxPanel(this, wxID_ANY, wxPoint(5, 39), wxSize(213, 47));
-	WxBoxSizer1->Add(WxPanel1, 1, wxEXPAND | wxALL, 5);
+	WxPanel1->SetBackgroundColour(wxColour(255, 255, 255));
 
-	WxBoxSizer2 = new wxBoxSizer(wxVERTICAL);
-	WxBoxSizer1->Add(WxBoxSizer2, 0, wxALIGN_TOP | wxALL, 5);
+	WxBoxSizer2 = new wxBoxSizer(wxHORIZONTAL);
+	WxBoxSizer2->Add(WxPanel1, 1, wxEXPAND | wxALL, 5);
+
+	
+	WxBoxSizer1->Add(WxBoxSizer2, 1, wxEXPAND | wxALL, 5);
+
+	WxBoxSizer3 = new wxBoxSizer(wxVERTICAL);
+	WxBoxSizer3->SetMinSize(wxSize(196, 350));
+	WxBoxSizer1->Add(WxBoxSizer3, 0, wxEXPAND, 5);
 
 	
 
 	WxButton1 = new wxButton(this, ID_WXBUTTON1, _("Zapis do pliku"), wxPoint(419, 15), wxSize(113, 27), 0, wxDefaultValidator, _("WxButton1"));
-	WxBoxSizer2->Add(WxButton1, 0, wxALIGN_CENTER | wxALL, 5);
+	WxBoxSizer3->Add(WxButton1, 0, wxALIGN_CENTER | wxALL, 5);
 
 	WxCheckBox1 = new wxCheckBox(this, ID_WXCHECKBOX1, _("Banan"), wxPoint(450, 57), wxSize(105, 19), 0, wxDefaultValidator, _("WxCheckBox1"));
-	WxBoxSizer2->Add(WxCheckBox1, 0, wxALIGN_CENTER | wxALL, 5);
+	WxBoxSizer3->Add(WxCheckBox1, 0, wxALIGN_CENTER | wxALL, 5);
 
 	WxScrollBar1 = new wxScrollBar(this, ID_WXSCROLLBAR1, wxPoint(400, 92), wxSize(150, 18), wxSB_HORIZONTAL, wxDefaultValidator, _("WxScrollBar1"));
-	WxBoxSizer2->Add(WxScrollBar1, 0, wxALIGN_CENTER | wxALL, 5);
-
 	WxScrollBar1->SetScrollbar(10, 15, 100, 10);
 	WxScrollBar1->Enable(false);
+	WxBoxSizer3->Add(WxScrollBar1, 0, wxALIGN_CENTER | wxALL, 5);
 
 	BtnColor = new wxButton(this, ID_BTNCOLOR, _("Kolor gwiazki"), wxPoint(419, 140), wxSize(113, 27), 0, wxDefaultValidator);
-	WxBoxSizer2->Add(BtnColor, 0, wxALIGN_CENTER | wxALL, 5);
+	WxBoxSizer3->Add(BtnColor, 0, wxALIGN_CENTER | wxALL, 5);
 
 	WxEdit1 = new wxTextCtrl(this, ID_WXEDIT1, _("tekst"), wxPoint(410, 182), wxSize(131, 30), 0, wxDefaultValidator, _("WxEdit1"));
-	WxBoxSizer2->Add(WxEdit1, 0, wxALIGN_CENTER | wxALL, 5);
+	WxBoxSizer3->Add(WxEdit1, 0, wxALIGN_CENTER | wxALL, 5);
 
 	wxArrayString arrayStringFor_WxChoice1;
 	WxChoice1 = new wxChoice(this, wxID_ANY, wxPoint(447, 21), wxSize(234, 30), arrayStringFor_WxChoice1, 0, wxDefaultValidator, _("WxChoice1"));
-	WxBoxSizer2->Add(WxChoice1, 0, wxALIGN_CENTER | wxALL, 5);
+	WxBoxSizer3->Add(WxChoice1, 0, wxALIGN_CENTER | wxALL, 5);
 
 	WxChoice1->Append("gwiazdka");
 	WxChoice1->Append("ksiê¿yc");
@@ -105,7 +113,9 @@ MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "GFK Lab03")
 	WxChoice1->SetSelection(0);
 	
 	
-
+	this->SetSizer(WxBoxSizer1);
+	this->Layout();
+	this->Centre(wxBOTH);
 
 
 
@@ -115,7 +125,9 @@ MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "GFK Lab03")
 	Bind(wxEVT_TEXT, &MyFrame::WxEdit1_Updated, this, ID_WXEDIT1);
 	Bind(wxEVT_CHECKBOX, &MyFrame::WxCheckBox1_Click, this, ID_WXCHECKBOX1);
 	Bind(wxEVT_SCROLL_THUMBTRACK, &MyFrame::WxScrollBar1_Scroll, this, ID_WXSCROLLBAR1);
+	Bind(wxEVT_PAINT, &MyFrame::Form_Paint, this);
 	Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+	
 }
 void MyFrame::OnExit(wxCommandEvent& event)
 {
@@ -139,5 +151,34 @@ void MyFrame::WxCheckBox1_Click(wxCommandEvent& e)
 
 void MyFrame::WxScrollBar1_Scroll(wxScrollEvent& e)
 {
-	WxStaticText1->SetLabel(wxString::Format("%d", WxScrollBar1->GetThumbPosition()));
+	//WxStaticText1->SetLabel(wxString::Format("%d", WxScrollBar1->GetThumbPosition()));
+}
+
+
+
+void MyFrame::Form_Paint(wxPaintEvent& event)
+{
+	int i, j;
+	wxPoint pts[5], ptsB[4];
+	wxPen MyPen;
+
+	pts[0] = wxPoint(200, 40);
+	pts[1] = wxPoint(220, 60);
+	pts[2] = wxPoint(240, 50);
+	pts[3] = wxPoint(260, 60);
+	pts[4] = wxPoint(280, 40);
+	ptsB[0] = wxPoint(400, 90);
+	ptsB[1] = wxPoint(415, 240);
+	ptsB[2] = wxPoint(480, 90);
+	ptsB[3] = wxPoint(520, 140);
+
+	wxPaintDC* MyDC;
+	MyDC = new wxPaintDC(this);
+
+
+	MyDC->DrawCircle(50, 260, 40);
+
+
+
+	delete (MyDC);
 }
