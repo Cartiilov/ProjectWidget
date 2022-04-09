79d213d95a48faf480d740d92a536c5f50a0b5bf
Cartiilov <92887460+Cartiilov@users.noreply.github.com>
2022-04-09 16:06:42 +0200

    proj2D final

---
+++ ProjectWidget/ChartClass.h
@@ -0,0 +1,34 @@
+#ifndef __ChartClass__
+#define __ChartClass__
+
+#include <memory>
+#include "ConfigClass.h"
+class Matrix;
+
+class ChartClass
+{
+    private:
+        std::shared_ptr<ConfigClass> cfg;
+        int x_step;         // liczba odcinkow z jakich bedzie sie skladal wykres
+        double x_min,x_max; // zakres zmiennej x
+        double y_min,y_max; // zakres wartosci przyjmowanych przez funkcje
+        double GetFunctionValue(double x); // zwraca wartosci rysowanej funkcji
+        
+        void line2d(Matrix t,double x1,double y1,double x2,double y2, int w, int h, wxDC* dc);
+        // powyzej sugerowana funkcja transformujaca odcinki (jesli ktos ma 
+        // inny pomysl mozna ja usunac)
+       
+    public:
+        ChartClass(std::shared_ptr<ConfigClass> c);
+        void Set_Range();   // ustala wartosci zmiennych x_min,y_min,x_max,y_max
+        double Get_Y_min(); // zwraca y_min
+        double Get_Y_max(); // zwraca y_max
+        void Draw(wxDC *dc, int w, int h);  // rysuje wykres
+        //Matrix Set_Rotate(double alpha);
+        Matrix Set_Scale(int w, int h);
+        Matrix Rotate(int w, int h);
+        Matrix Move_Via_Axis();
+        void RotateText(wxDC* DC, Matrix trans_matrix, wxString str, double x, double y, int w, int h);
+};
+
+#endif
